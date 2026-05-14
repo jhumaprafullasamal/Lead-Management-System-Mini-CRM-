e657dbad1ea3a40c1121d37bf1d1313629f49db5
jhumaprafullasamal <jhumaprafullasamal@gmail.com>
2026-05-14 21:37:07 +0530

    fix loading

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '',
+  baseURL: process.env.REACT_APP_API_URL || 'https://lead-management-system-mini-crm-1.onrender.com',
   timeout: 10000,
 });
 
